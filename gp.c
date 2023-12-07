6557e7d692f3c54df458d1d4f183b668dfd6b3f8
student-akriti123 <akritisinha1602@gmail.com>
2023-12-07 13:00:41 +0530

    first commit

---
+++ gp.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("Enter the value n\n");
+ scanf("%d",&n);
+
+ int a =1;
+ for(int i=0;i<=n;i++)
+    {
+       printf("%d\n",a);
+       a=a*2;
+
+    }
+
+
+
+ return 0;
+}
